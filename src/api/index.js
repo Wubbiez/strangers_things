b5bbdf3a4a01724aacf727ae298de2ed98217893
Wubbiez <108950491+Wubbiez@users.noreply.github.com>
2022-12-14 21:35:17 -0500

    sthings - register user

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-export const BASE_URL = 'https://strangers-things.herokuapp.com';
+export const BASE_URL = "https://strangers-things.herokuapp.com";
 
-export async function getPersonalInfo() {
+// export async function getPersonalInfo() {
+//   try {
+//     const response = await fetch(`${BASE_URL}/api/2209-ftb-et-web-ps/users/me`);
+//   }
+// }
+
+export async function registerUser(username, password) {
   try {
-    const response = await fetch(`${BASE_URL}/api/2209-ftb-et-web-ps/users/me`);
-  }
-}
-
-export async function registerUser(username,password) {
-  try{
-    const response = fetch('https://strangers-things.herokuapp.com/api/COHORT-NAME/users/register', {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        user: {
-          username: username,
-          password: password
-        }
-      })
-  }
-} catch (e) {
+    const response = await fetch(
+      "https://strangers-things.herokuapp.com/api/COHORT-NAME/users/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: {
+            username: username,
+            password: password,
+          },
+        }),
+      }
+    );
+    const results = await response.json();
+    console.log(results);
+    console.log(results.data.token);
+    if (results.data.token) {
+      localStorage.setItem("jwt", results.data.token);
+    }
+  } catch (e) {
+    throw ("err", e);
   }
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,34 @@
 import React from "react";
+import {registerUser} from "../api";
 
 function Signup(props) {
-  const { username, setUsername, password, setPassword } = props;
+  const {username, setUsername, password, setPassword} = props;
+
+  async function buttonHandle() {
+    await registerUser(username, password);
+  }
+
   return (
-    <div>
-      <h1>This is the Sign-up page</h1>
-    </div>
+      <div>
+        <h1>This is the Sign-up page</h1>
+        <input
+            placeholder="Enter Your Username"
+            value={username}
+            onChange={async (event) => {
+              event.preventDefault();
+              setUsername(event.target.value);
+            }}
+        ></input>
+        <input
+            placeholder="Enter Your Password"
+            value={password}
+            onChange={async (event) => {
+              event.preventDefault();
+              setPassword(event.target.value);
+            }}
+        ></input>
+        <button onClick={buttonHandle}>Submit</button>
+      </div>
   );
 }
 
