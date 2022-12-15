f91fd449fbbdbdfcc04bc5ad93a49d679c655ada
Wubbiez <108950491+Wubbiez@users.noreply.github.com>
2022-12-14 20:42:12 -0500

    sthings

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,21 @@ export async function getPersonalInfo() {
     const response = await fetch(`${BASE_URL}/api/2209-ftb-et-web-ps/users/me`);
   }
 }
+
+export async function registerUser(username,password) {
+  try{
+    const response = fetch('https://strangers-things.herokuapp.com/api/COHORT-NAME/users/register', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password
+        }
+      })
+  }
+} catch (e) {
+  }
+}
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Signup(props) {
+  const { username, setUsername, password, setPassword } = props;
   return (
     <div>
       <h1>This is the Sign-up page</h1>
