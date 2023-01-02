bba858a201e272efec12a2182a4bfa460b0ca894
Wubbiez <108950491+Wubbiez@users.noreply.github.com>
2023-01-01 20:07:15 -0500

    Finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,16 @@ function Home() {
         <h1>Welcome to Stranger's Things!</h1>
       </div>
 
-      <div>
+      <div className={styles.welcome}>
         <p>
           This is a social website where you can buy and sell goods from
           strangers! Not to be confused with the Netflix original series
           (besides the similar logo).
         </p>
+        <p>Please create an account or log-in to begin!</p>
       </div>
+
+      <div className={styles.intro}></div>
     </div>
   );
 }
